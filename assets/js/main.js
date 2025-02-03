ecfcba526f650988f55b6a3cc7e3c3235a832a3a
Rolando Valle <lancevalle8@gmai.com>
2025-02-03 19:11:38 +0800

    feat : add scroll sections and active link
    - Implement JavaScript functionality for scroll detection and active links
    - Highlight the navigation link corresponding to the current section

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,7 +52,38 @@ const swiper = new Swiper('.services__swiper', {
   });
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up')
+  //when the scroll is higher than 350 viewport height, add the
+  this.scrollY >= 350? scrollUp.classList.add('show-scroll')
+  : scrollUp.classList.remove('show-scroll')
+}
+
+window.addEventListener('scroll',scrollUp)
+
+scrollUp()
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+    sectionTop = current.offsetTop - 58,
+    sectionId = current.getAttribute('id'),
+    sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+    if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+      sectionsClass.classList.add('active-link')
+    }else{
+      sectionsClass.classList.remove('active-link')
+    }
+  })
+}
+
+window.addEventListener('scroll', scrollActive)
+scrollActive()
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
